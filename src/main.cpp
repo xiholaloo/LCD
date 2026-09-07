bd8c3535df9665911d67d2b92baad703e7c18386
Jiva <xiholaloo@gmail.com>
2026-09-07 14:13:30 +0700

    added DHT22 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,40 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // 2 tipe hexa, default 0x27. 0x3f (optional)
+#define DHTPin 25
+#define DHTTYPE DHT22
+
+DHT dht(25, DHT22);
 
 void setup() {
   Wire.begin(21,22);
   lcd.init();
   lcd.backlight();
-
+  dht.begin();
 }
 
 void loop(){
-  lcd.clear();
-  lcd.setCursor(7,0);
-  lcd.print("BLK");
-  lcd.setCursor(4,1);
-  lcd.print("WONOJATI");
-  delay(2000);
-  
-  lcd.setCursor(7,0);
-  lcd.print("IOT");
-  lcd.setCursor(1,1);
-  lcd.print("SMART FARMING");
-  delay(2000);
+  float suhu = dht.readTemperature();
+  float kelembaban = dht.readHumidity();
+
+  if (isnan(suhu) || isnan (kelembaban)) {
+    lcd.setCursor(3,0);
+    lcd.print("Gagal Membaca");
+    lcd.setCursor(3,1);
+    lcd.print("Sensor");
+    return;
+  }
+
+  lcd.setCursor(0,0);
+  lcd.print("Suhu:");
+  lcd.print(suhu);
+  lcd.print("°C");
+  lcd.setCursor(0,1);
+  lcd.print("kelembaban:");
+  lcd.print(kelembaban);
+  lcd.print("%");
+  delay(1000);
 }
